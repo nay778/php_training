15ca92dce79865f8d044ee19df41e91589c9c739
nay778 <naynaingw@gmail.com>
2021-12-15 15:39:05 +0630

    [master] updated remove line space

--- tutorials/tutorial_01/index.php
+++ tutorials/tutorial_01/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,27 +9,23 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <table>
         <?php
-            for ($row = 1; $row <= 8; $row++) {
-
-                echo "<tr>";
-                for ($col = 1; $col <= 8; $col++) {
-
-                    if (($row + $col) % 2 == 0){
-
-                        echo "<td> </td>";
-
-                    } else {
-
-                        echo "<td class='black'> </td>";
-
-                    }
+        for ($row = 1; $row <= 8; $row++) {
+            echo "<tr>";
+            for ($col = 1; $col <= 8; $col++) {
+                if (($row + $col) % 2 == 0) {
+                    echo "<td> </td>";
+                } else {
+                    echo "<td class='black'> </td>";
                 }
-                echo "</tr>";
             }
+            echo "</tr>";
+        }
         ?>
     </table>
 </body>
+
 </html>
--- tutorials/tutorial_02/index.php
+++ tutorials/tutorial_02/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,45 +9,34 @@
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-        <?php
-        $n = 6;
-            for ($row = 1; $row <= $n; $row++) {
-
-                for ($col = 1; $col <= (2*$n - 1); $col++) {
-
-                   if ($col >= $n-($row-1) && $col <= $n+($row-1)){
-                        echo "&nbsp;";
-                        echo "*";
-
-                   } else {
-                       
-                    echo "&nbsp;&nbsp;&nbsp;";
-
-                   } 
-                }
-
-                echo "<br>";
+    <?php
+    $n = 6;
+    for ($row = 1; $row <= $n; $row++) {
+        for ($col = 1; $col <= (2 * $n - 1); $col++) {
+            if ($col >= $n - ($row - 1) && $col <= $n + ($row - 1)) {
+                echo "&nbsp;";
+                echo "*";
+            } else {
+                echo "&nbsp;&nbsp;&nbsp;";
             }
+        }
+        echo "<br>";
+    }
 
-            for ($row = $n-1; $row >= 1; $row--) {
-    
-                for ($col = 1; $col <= (2*$n - 1); $col++) {
-    
-                   if ($col >= $n-($row-1) && $col <= $n+($row-1)){
-                        echo "&nbsp;";
-                        echo "*";
-    
-                   } else {
-                           
-                    echo "&nbsp;&nbsp;&nbsp;";
-    
-                   } 
-                }
-    
-                echo "<br>";
+    for ($row = $n - 1; $row >= 1; $row--) {
+        for ($col = 1; $col <= (2 * $n - 1); $col++) {
+            if ($col >= $n - ($row - 1) && $col <= $n + ($row - 1)) {
+                echo "&nbsp;";
+                echo "*";
+            } else {
+                echo "&nbsp;&nbsp;&nbsp;";
             }
-
-        ?>
+        }
+        echo "<br>";
+    }
+    ?>
 </body>
+
 </html>
