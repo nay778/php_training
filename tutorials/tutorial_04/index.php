38fd17408b91817eac5adc7f97e34e250693d6eb
nay778 <naynaingw@gmail.com>
2021-12-16 22:56:51 +0630

    [master] updated login system design

--- tutorials/tutorial_04/index.php
+++ tutorials/tutorial_04/index.php
@@ -6,20 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login System</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <div>
-        <h1>Login</h1>
+    <div class="wrapper">
+        <h1>Welcome</h1>
         <?php if (isset($_GET['incorrect'])) : ?>
-            <div>
+            <div class="error">
                 Incorrect Email or Password
             </div>
         <?php endif ?>
         <form action="login.php" method="post">
-            <input type="email" name="email" placeholder="Email" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <button type="submit" >
+            <input type="email" name="email" placeholder="Email" required><br>
+            <input type="password" name="password" placeholder="Password" required><br>
+            <button type="submit" class="btn">
                 Login
             </button>
         </form>
--- tutorials/tutorial_04/profile.php
+++ tutorials/tutorial_04/profile.php
@@ -13,10 +13,12 @@ if (!isset($_SESSION['auth'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <div>
+    <div class="wrapper">
         <h1><?= $_SESSION['auth']["username"] ?></h1>
         <ul>
             <li>
